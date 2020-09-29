228c678cd040d2e7abfe1ec5a90adc10949f9d45
IgolJack <iegolepic@gmail.com>
2020-09-29 16:34:10 +0300

    calendar

--- src/components/CalendarPage.js
+++ src/components/CalendarPage.js
@@ -3,17 +3,11 @@ import Calendar from './calendar/calendar'
 import {
     Link
 } from 'react-router-dom'
-import Button from "@material-ui/core/Button";
-
 
  class CalendarPage extends React.Component {
     render() {
         return (
-            <div className="CalendarPage">
-
                 <Calendar/>
-
-            </div>
         )
     }
 }
--- src/components/calendar/calendar.js
+++ src/components/calendar/calendar.js
@@ -4,6 +4,7 @@ import {db} from "../services/firebase";
 import TextField from "@material-ui/core/TextField";
 import Button from "@material-ui/core/Button";
 import {Link} from "react-router-dom";
+
 import FullCalendar from "@fullcalendar/react";
 import dayGridPlugin from "@fullcalendar/daygrid";
 import interactionPlugin from "@fullcalendar/interaction";
