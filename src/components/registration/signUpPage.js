4faedf5bfd97f53988612e503132506037ba0c2c
IgolJack <iegolepic@gmail.com>
2020-10-09 01:19:13 +0300

    NiceviewOfSignUp

--- src/components/registration/signUpPage.js
+++ src/components/registration/signUpPage.js
@@ -1,17 +1,24 @@
 import React, { useCallback, useContext, useState } from "react";
 import { auth, db } from '../firebase/firebase'
 import {AuthContext} from '../login/Auth'
-import { Form, Input, Button, InputNumber, Select, Divider } from 'antd';
+import { Form, Input, Button, message , Select, Divider } from 'antd';
 import BackToHome from '../UI/backToHome'
 import { UserOutlined, LockOutlined } from '@ant-design/icons';
+import './Box.css';
+
+
+
+
 const { Option } = Select;
 
+
+
 const layout = {
-    labelCol: { span: 8 },
-    wrapperCol: { span: 16 },
+    labelCol: { span: 0},
+    wrapperCol: { span: 0 },
   };
   const tailLayout = {
-    wrapperCol: { offset: 8, span: 16 },
+    wrapperCol: { offset: 0, span: 0 },
   };
 
 const SignUpPage = () => {
@@ -29,6 +36,7 @@ let equipment = '0'
 let discharges = '0'
 let count = '0'
 let course = 'Первый курс'
+
 
   const { currentUser } = useContext(AuthContext);
   const [form] = Form.useForm()
@@ -77,6 +85,28 @@ let course = 'Первый курс'
     }    
       
   };
+  const succes = () => {
+    message.success('Студент успешно создан', 2);
+    form.setFieldsValue({password: Password.generate(8)})
+    form.setFieldsValue({email: ''})
+    form.setFieldsValue({name: ''})
+    form.setFieldsValue({lateness: '0'})
+    form.setFieldsValue({lvl: '0'})
+    form.setFieldsValue({missed: '0'})
+    form.setFieldsValue({disgrace: '0'})
+    form.setFieldsValue({responsible: '0'})
+    form.setFieldsValue({concert: '0'})
+    form.setFieldsValue({equipment: '0'})
+    form.setFieldsValue({count: '0'})
+    form.setFieldsValue({course: ''})
+    form.setFieldsValue({equipment: '0'})
+    form.setFieldsValue({discharges: '0'})
+  };
+  const succesRed = () => {
+    message.info('Регистрация прошла успешно');
+  };
+
+
 
     const handleLogin = useCallback(
       
@@ -85,7 +115,7 @@ let course = 'Первый курс'
             await auth.createUserWithEmailAndPassword(values.email, values.password)
             .then(function(result)
             { 
-              console.log('Регистрация прошла успешно.')
+              succesRed()
               return result.user.updateProfile({
               displayName: values.name
             })}
@@ -105,10 +135,12 @@ let course = 'Первый курс'
                         equipment : (form.getFieldValue("equipment")),
                         count : (form.getFieldValue("count")),
                         course: (form.getFieldValue("course")),
+                        discharges: (form.getFieldValue("discharges")),
                         uid: currentUser.uid,
                       })
                       .then(function() {
-                        console.log("Студент успешно создан.");}
+                        succes()
+                      }
                       )} 
         catch (error) {
             console.log(error);
@@ -117,28 +149,28 @@ let course = 'Первый курс'
     );
 
       return (
-        <div  style={{padding:'50px'}}>
+        <div  style={{padding:'50px',  width: "100%" }}>
           <BackToHome/>
-        <br/>
+       <br/>
         <Form
+          
           form={form}
+          layout="auto"
           size="large"
-          style={{maxWidth:'500px'}}
           {...layout}
           name="basic"
-          initialValues={{ 
-
-           }}
           onFinish={handleLogin}
         >
+          
+          
           <Form.Item
             name="email"
             value={email}
-            rules={[{ required: true, message: 'Please input your email!' }]}
+            rules={[{ required: true, message: 'Введите email!' }]}
           >
             <Input addonBefore="Email" prefix={<UserOutlined className="site-form-item-icon" />} name="email"/>
           </Form.Item>
-    
+
           <Form.Item
             name="password"
             value='password'
@@ -152,7 +184,6 @@ let course = 'Первый курс'
           </Form.Item>
 
          
-
           <Form.Item
             name="name"
             value={name}
@@ -160,100 +191,8 @@ let course = 'Первый курс'
           >
             <Input addonBefore="ФИО" name="name"/>
           </Form.Item>
-
+       
           <Form.Item
-            label="Опазданий"
-            type="number"
-            name="lateness"
-            value={lateness}
-            rules={[{ required: true, message: 'Please input your email!' }]}
-          >
-            <InputNumber  name="lateness" min={0} defaultValue={lateness} />
-          </Form.Item>
-
-          <Form.Item
-            label="Уровень"
-            type="number"
-            name="lvl"
-            value={lvl}
-            rules={[{ required: true, message: 'Please input your email!' }]}
-          >
-            <InputNumber  name="lvl" min={0} defaultValue={lvl} />
-          </Form.Item>
-
-          <Form.Item
-            label="Пропусков"
-            type="number"
-            name="missed"
-            value={missed}
-            rules={[{ required: true, message: 'Please input your email!' }]}
-          >
-            <InputNumber name="missed" defaultValue={missed} min={0} />
-          </Form.Item>
-
-          <Form.Item
-            label="Опозорил раз"
-            type="number"
-            name="disgrace"
-            value={disgrace}
-            rules={[{ required: true, message: 'Please input your email!' }]}
-          >
-            <InputNumber name="disgrace" defaultValue={disgrace} min={0} />
-          </Form.Item>
-
-          <Form.Item
-            label="Отвественен раз"
-            type="number"
-            name="responsible"
-            value={responsible}
-            rules={[{ required: true, message: 'Please input your email!' }]}
-          >
-            <InputNumber name="responsible" defaultValue={responsible} min={0} />
-          </Form.Item>
-
-          <Form.Item
-            label="Мероприятий посетило"
-            type="number"
-            name="concert"
-            value={concert}
-            rules={[{ required: true, message: 'Please input your email!' }]}
-          >
-            <InputNumber name="concert" defaultValue={concert} min={0} />
-          </Form.Item>
-
-          <Form.Item
-            label="Оборудование брало"
-            type="number"
-            name="equipment"
-            value={equipment}
-            rules={[{ required: true, message: 'Please input your email!' }]}
-          >
-            <InputNumber name="equipment" defaultValue={equipment} min={0} />
-          </Form.Item>
-          
-          <Form.Item
-            label="Выписываний"
-            type="number"
-            name="discharges"
-            value={discharges}
-            rules={[{ required: true, message: 'Please input your email!' }]}
-          >
-            <InputNumber name="discharges" defaultValue={discharges} min={0} />
-          </Form.Item>
-          
-          <Form.Item
-            label="Сдано записей"
-            type="number"
-            name="count"
-            value={count}
-            rules={[{ required: true, message: 'Please input your email!' }]}
-          >
-            <InputNumber name="count" defaultValue={count} min={0} />
-          </Form.Item>
-
-
-          <Form.Item
-            label="Выберите курс"
             type="number"
             name="course"
             value={course}
@@ -262,6 +201,7 @@ let course = 'Первый курс'
            placeholder="Выберите курс" 
            name="course"
            value={course}
+           addonBefore="ФИО"
           >
            <Option value="Первый курс">Первый курс</Option>
            <Option value="Второй курс">Второй курс</Option>
@@ -271,14 +211,92 @@ let course = 'Первый курс'
           
          </Select>
          </Form.Item>
-        
-          
+         <Divider type="horizontal" style={{ width: "100%"}} />
 
-          <Form.Item {...tailLayout}>
-          <Button type="primary" htmlType="submit">
+          <Form.Item
+            name="lateness"
+            value={lateness}
+            rules={[{ required: true, message: 'Please input your email!' }]}
+          >
+            <Input type="number" addonBefore="Опазданий" name="lateness" min={0} defaultValue={lateness} />
+          </Form.Item>
+          <Form.Item
+            name="lvl"
+            value={lvl}
+            rules={[{ required: true, message: 'Please input your email!' }]}
+          >
+            <Input type="number" addonBefore="Уровень" name="lvl" min={0} defaultValue={lvl} />
+          </Form.Item>
+         
+          <Form.Item
+            name="missed"
+            value={missed}
+            rules={[{ required: true, message: 'Please input your email!' }]}
+          >
+            <Input name="missed" type="number" addonBefore="Пропусков" defaultValue={missed} min={0} />
+          </Form.Item>
+         
+          <Form.Item
+            name="disgrace"
+            value={disgrace}
+            rules={[{ required: true, message: 'Please input your email!' }]}
+          >
+            <Input type="number" addonBefore="Опозорил" name="disgrace" defaultValue={disgrace} min={0} />
+          </Form.Item>
+         
+          <Form.Item
+            name="responsible"
+            value={responsible}
+            rules={[{ required: true, message: 'Please input your email!' }]}
+          >
+            <Input type="number" addonBefore="Отвественен" name="responsible" defaultValue={responsible} min={0} />
+          </Form.Item>
+          
+          <Form.Item
+            name="concert"
+            value={concert}
+            rules={[{ required: true, message: 'Please input your email!' }]}
+          >
+            <Input type="number" addonBefore="Мероприятий" name="concert" defaultValue={concert} min={0} />
+          </Form.Item>
+        
+          <Form.Item
+            name="equipment"
+            value={equipment}
+            rules={[{ required: true, message: 'Please input your email!' }]}
+          >
+            <Input type="number" addonBefore="Оборудование" name="equipment" defaultValue={equipment} min={0} />
+          </Form.Item>
+          
+          <Form.Item
+            name="discharges"
+            value={discharges}
+            rules={[{ required: true, message: 'Please input your email!' }]}
+          >
+            <Input type="number" addonBefore="Выписываний" name="discharges" defaultValue={discharges} min={0} />
+          </Form.Item>
+        
+          <Form.Item
+            name="count"
+            value={count}
+            rules={[{ required: true, message: 'Please input your email!' }]}
+          >
+            <Input type="number" addonBefore="Записей" name="count" defaultValue={count} min={0} />
+          </Form.Item>
+
+
+          
+       
+          <Divider type="horizontal" style={{ width: "100%"}} />
+
+          <Form.Item 
+          {...tailLayout} 
+          >
+          <Button style={{width:'100%'}} type="primary" htmlType="submit">
               Зарегистрировать студента
             </Button>
           </Form.Item>
+
         </Form>
         </div>
       );
