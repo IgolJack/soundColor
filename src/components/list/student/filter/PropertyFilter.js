3c0266881a585c9bb2c14bfddf24457da5dfe620
Linus-Seal <Linus-Seal@users.noreply.github.com>
2020-10-09 10:52:29 +0300

    some filters design

--- src/components/list/student/filter/PropertyFilter.js
+++ src/components/list/student/filter/PropertyFilter.js
@@ -2,8 +2,9 @@ import React, { Component } from 'react';
 
 import { Collapse } from 'antd';
 import { Select } from 'antd';
-const { Panel } = Collapse;
+import { InputNumber } from 'antd';
 
+const { Panel } = Collapse;
 const { Option } = Select;
 
 class PropertyFilter extends Component {
@@ -16,48 +17,49 @@ class PropertyFilter extends Component {
                         <p>
                             <h6>Курс</h6>
                             <Select
+                                fullWidth
                                 name="course"
                                 labelId="demo-simple-select-label"
                                 id="demo-simple-select"
-
+                                defaultValue=""
                                 onChange={this.props.onInputChange}
                             >
-                                <option value="">Все курсы</option>
-                                <option value="Первый курс">Первый курс</option>
-                                <option value="Второй курс">Второй курс</option>
-                                <option value="Третий курс">Третий курс</option>
-                                <option value="Четвертый курс">Четвертый курс</option>
-                                <option value="Пятый курс">Пятый курс</option>
+                                <Option value="">Все курсы</Option>
+                                <Option value="Первый курс">Первый курс</Option>
+                                <Option value="Второй курс">Второй курс</Option>
+                                <Option value="Третий курс">Третий курс</Option>
+                                <Option value="Четвертый курс">Четвертый курс</Option>
+                                <Option value="Пятый курс">Пятый курс</Option>
                             </Select>
                             </p>
                             <p>
                             <h6>Уровень</h6>
                             <Select
-
+                                fullWidth
                                 name="lvl"
                                 labelId="demo-simple-select-label"
                                 id="demo-simple-select"
-
+                                defaultValue=""
                                 onChange={this.props.onInputChange}
                             >
-                                <option value="">Все уровни</option>
-                                <option value="1">Первый уровень</option>
-                                <option value="2">Второй уровень</option>
-                                <option value="3">Третий уровень</option>
-                                <option value="4">Четвертый уровень</option>
-                                <option value="5">Пятый уровень</option>
+                                <Option value="">Все уровни</Option>
+                                <Option value="1">Первый уровень</Option>
+                                <Option value="2">Второй уровень</Option>
+                                <Option value="3">Третий уровень</Option>
+                                <Option value="4">Четвертый уровень</Option>
+                                <Option value="5">Пятый уровень</Option>
                             </Select>
                             </p>
                             <p>
                                 <h6>Пропуски</h6>
-                            <input
+                            <InputNumber
                                 id="standard-full-width"
                                 label="Пропуски"
                                 helperText="Значение и более"
                                 type="number"
                                 fullWidth
                                 name="missed"
-
+                                min={0}
                                 onChange={this.props.onInputChange}
                             />
                         </p>
--- src/components/list/student/filter/SearchFilter.js
+++ src/components/list/student/filter/SearchFilter.js
@@ -1,16 +1,18 @@
 import React, { Component } from 'react';
+import { Input } from 'antd';
 
 class SearchFilter extends Component {
     
     render() {
         return (
             <div style={{ margin: 12 }}>
-                <input
+                <Input
                     id="outlined-basic"
                     name="searchStudent"
-                    label="Найти студента"
+                    placeholder="Найти студента"
                     variant="outlined"
                     size="small"
+                    fullWidth
                     value={this.props.searchStudent}
                     onChange={this.props.search}
                 />
