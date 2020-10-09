e92d4c2b515d783ca4bbe397cb44cf1c923d4c28
IgolJack <iegolepic@gmail.com>
2020-10-09 16:39:45 +0300

    nice loading!

--- src/components/login/Auth.js
+++ src/components/login/Auth.js
@@ -1,6 +1,10 @@
 import React, { useEffect, useState } from "react";
 import { auth } from '../firebase/firebase'
+import { SmileTwoTone} from '@ant-design/icons';
+import { Spin } from 'antd';
+
 export const AuthContext = React.createContext();
+const antIcon = <SmileTwoTone style={{ fontSize: 50 }} spin />;
 
 export const AuthProvider = ({ children }) => {
   const [currentUser, setCurrentUser] = useState(null);
@@ -14,7 +18,7 @@ export const AuthProvider = ({ children }) => {
   }, []);
 
   if(pending){
-    return <>Loading...</>
+    return <div style={{width:'100%', height: '100%', position: 'fixed', top: '0', left: '0', display:'flex', alignItems: 'center', alignContent: "center", justifyContent:'center', overflow: 'auto'}}><div><Spin indicator={antIcon} /></div></div>
   }
 
   return (
